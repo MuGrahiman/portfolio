4e7fb85265aa235333397e4d37a0b177b8fc429f
MuGrahiman <mujeebrahiman2000@gmail.com>
2023-08-23 22:03:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+//  const icons = document.querySelectorAll('.mern-stack-icon');
+
+//         icons.forEach(icon => {
+//             icon.addEventListener('mouseenter', () => {
+//                 const offsetX = Math.random() * 200 - 100;
+//                 const offsetY = Math.random() * 200 - 100;
+//                 icon.style.transform = `translate(${offsetX}px, ${offsetY}px) scale(1.2)`;
+//             });
+
+//             icon.addEventListener('mouseleave', () => {
+//                 icon.style.transform = 'scale(1)';
+//             });
+//         });
